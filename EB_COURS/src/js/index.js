3f28615026c55cef47ad316e3bc2003b028c86cf
Alya Antsiperovich <antsiperovich2002@mail.ru>
2023-10-05 02:24:32 +0300

    mod promo

--- EB_COURS/src/js/index.js
+++ EB_COURS/src/js/index.js
@@ -2,14 +2,30 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 5,
     spaceBetween: 62,
     freeMode: true,
+    
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
     pagination: {
-      el: ".swiper-pagination",
+      el: ".swiper-paginationn",
       clickable: true,
     },
     mousewheel: true,
     keyboard: true,
+  });
+
+
+  var swiper6 = new Swiper(".mySwiper6", {
+    effect: "cards",
+    grabCursor: true,
+    autoplay: {
+      delay: 4000, // Задержка в миллисекундах между сменой слайдов (в данном случае, 2000 мс = 2 секунды)
+    },
+    loop: true, // Включить бесконечный цикл слайдера
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
